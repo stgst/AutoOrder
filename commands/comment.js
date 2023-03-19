ce94de0c3bf322c17bc2d1fc33c901c32d08c05a
xiunG <steven19790906@gmail.com>
2023-03-19 18:14:18 +0800

    Delete comment.js

--- commands/comment.js
+++
@@ -1,6 +0,0 @@
-const {SlashCommandBuilder, ActionRowBuilder, TextInputBuilder, ModalBuilder, TextInputStyle} = require('discord.js')
-
-module.exports = SlashCommandBuilder()
-    .setName("comment")
-    .setDescription("評價")
-    .
